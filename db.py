1a3fda6ef1232298a249e7cc6008d70248953160
codykrug <39412797+codykrug@users.noreply.github.com>
2018-11-14 08:22:46 -0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect('Homes.db')
+
+c = conn.cursor()
+
+createHomes = "CREATE TABLE Homes(Parcel TEXT, Name TEXT, Address TEXT, LandUseCode INT, SaleDate DATE, SalePrice INT, TotalRecordCount INT)"
+
+c.execute(createHomes)
+
+#read file
+
+data = open('os.txt', 'r')
+
+#bulk insert
+
+con.executemany("insert into Homes(Parcel, Name, Address, LandUseCode, SaleDate, SalePrice, TotalRecordCount) values (?,?,?,?,?,?,?)", Homes)
+
+conn.close()
